df0e2b943991464418874b3a28931b465c509f67
Jaeyeong Baek <jaeyeong.b@gmail.com>
2017-08-21 23:28:18 +0900

    Init Project

---
+++ src/main/java/com/example/noel/NoelApplication.java
@@ -0,0 +1,12 @@
+package com.example.noel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoelApplication.class, args);
+	}
+}
